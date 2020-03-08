ef7b29b17d9940e317b92f84d591aa31f7859a92
cnvillarta <48200320+cnvillarta@users.noreply.github.com>
2020-03-08 20:18:17 +0800

    unsaved changes alert (cancel action)

---
+++ AIDS GUI/Administator/Update Pilot/UnsavedChangesAlert.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'UnsavedChangesAlert.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(531, 191)
+        Dialog.setMinimumSize(QtCore.QSize(531, 191))
+        Dialog.setMaximumSize(QtCore.QSize(531, 191))
+        self.btn_cancel = QtWidgets.QPushButton(Dialog)
+        self.btn_cancel.setGeometry(QtCore.QRect(70, 140, 151, 31))
+        self.btn_cancel.setStyleSheet("QPushButton {\n"
+"color: rgb(0, 0, 0);\n"
+"background-color: rgb(201, 201, 201);\n"
+"border: 1.2px solid #ABABAB;\n"
+"outline: none;}\n"
+"\n"
+"QPushButton:hover{\n"
+"color: rgb(255, 255, 255);\n"
+"background-color: rgb(171, 171, 171);\n"
+"outline: none;\n"
+"border: none;\n"
+"}\n"
+"\n"
+"QPushButton:pressed{\n"
+"color: rgb(255, 255, 255);\n"
+"background-color: rgb(129, 129, 129);\n"
+"outline: none;\n"
+"border: none;\n"
+"}\n"
+"\n"
+"\n"
+"")
+        self.btn_cancel.setObjectName("btn_cancel")
+        self.btn_delete = QtWidgets.QPushButton(Dialog)
+        self.btn_delete.setGeometry(QtCore.QRect(310, 140, 151, 31))
+        self.btn_delete.setStyleSheet("QPushButton {\n"
+"background-color: rgb(255, 176, 6);\n"
+"border: 1.2px solid #ff9d07;\n"
+"outline: none;\n"
+"}\n"
+"\n"
+"QPushButton:hover{\n"
+"background-color: rgb(255, 157, 7);\n"
+"outline: none;\n"
+"border: none;\n"
+"}\n"
+"\n"
+"QPushButton:pressed{\n"
+"background-color: rgb(254, 140, 8);\n"
+"outline: none;\n"
+"border: none;\n"
+"}")
+        self.btn_delete.setObjectName("btn_delete")
+        self.layoutWidget = QtWidgets.QWidget(Dialog)
+        self.layoutWidget.setGeometry(QtCore.QRect(30, 19, 471, 91))
+        self.layoutWidget.setObjectName("layoutWidget")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.layoutWidget)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.label = QtWidgets.QLabel(self.layoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Helvetica")
+        font.setPointSize(21)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.verticalLayout_2.addWidget(self.label)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label_2 = QtWidgets.QLabel(self.layoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Helvetica")
+        font.setPointSize(16)
+        self.label_2.setFont(font)
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout.addWidget(self.label_2)
+        self.label_3 = QtWidgets.QLabel(self.layoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Helvetica")
+        font.setPointSize(16)
+        self.label_3.setFont(font)
+        self.label_3.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_3.setObjectName("label_3")
+        self.verticalLayout.addWidget(self.label_3)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.btn_cancel.setText(_translate("Dialog", "Cancel"))
+        self.btn_delete.setText(_translate("Dialog", "Discard Changes"))
+        self.label.setText(_translate("Dialog", "<html><head/><body><p><span style=\" color:#2c365d;\">Unsaved Changes</span></p></body></html>"))
+        self.label_2.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:13pt; color:#2c365d;\">If you leave this page, any changes you have made will be lost.</span></p></body></html>"))
+        self.label_3.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:13pt; color:#2c365d;\">Are you sure you want to leave this page?</span></p></body></html>"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
