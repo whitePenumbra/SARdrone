12bfc3a1daee8f09b8433e4a69d3b4b19284baba
cnvillarta <48200320+cnvillarta@users.noreply.github.com>
2020-04-28 14:37:54 +0800

    error/success alert

---
+++ Gui/Administrator/AddPilot/AddPilotError.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'AddPilotError.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(531, 191)
+        Dialog.setMinimumSize(QtCore.QSize(531, 191))
+        Dialog.setMaximumSize(QtCore.QSize(531, 191))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("../../Resources/logo_svg.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        Dialog.setWindowIcon(icon)
+        # removes ? from title bar
+        Dialog.setWindowFlags(QtCore.Qt.WindowSystemMenuHint | QtCore.Qt.WindowTitleHint | QtCore.Qt.WindowCloseButtonHint)
+        self.btn_OK = QtWidgets.QPushButton(Dialog)
+        self.btn_OK.setGeometry(QtCore.QRect(200, 140, 131, 31))
+        self.btn_OK.setMinimumSize(QtCore.QSize(131, 31))
+        self.btn_OK.setMaximumSize(QtCore.QSize(131, 31))
+        self.btn_OK.setStyleSheet("QPushButton {\n"
+"color: rgb(0, 0, 0);\n"
+"background-color: rgb(201, 201, 201);\n"
+"border: 1.2px solid #ABABAB;\n"
+"outline: none;}\n"
+"\n"
+"QPushButton:hover{\n"
+"color: rgb(255, 255, 255);\n"
+"background-color: rgb(171, 171, 171);\n"
+"outline: none;\n"
+"border: none;\n"
+"}\n"
+"\n"
+"QPushButton:pressed{\n"
+"color: rgb(255, 255, 255);\n"
+"background-color: rgb(129, 129, 129);\n"
+"outline: none;\n"
+"border: none;\n"
+"}\n"
+"\n"
+"\n"
+"")
+        self.btn_OK.setObjectName("btn_OK")
+        self.widget = QtWidgets.QWidget(Dialog)
+        self.widget.setGeometry(QtCore.QRect(20, 9, 491, 121))
+        self.widget.setObjectName("widget")
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.widget)
+        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.label = QtWidgets.QLabel(self.widget)
+        self.label.setText("")
+        self.label.setPixmap(QtGui.QPixmap("../../Resources/error.png"))
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.verticalLayout_3.addWidget(self.label)
+        self.lbl_error = QtWidgets.QLabel(self.widget)
+        font = QtGui.QFont()
+        font.setFamily("Helvetica")
+        font.setPointSize(19)
+        self.lbl_error.setFont(font)
+        self.lbl_error.setAlignment(QtCore.Qt.AlignCenter)
+        self.lbl_error.setObjectName("lbl_error")
+        self.verticalLayout_3.addWidget(self.lbl_error)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Add Pilot Error"))
+        self.btn_OK.setText(_translate("Dialog", "Dismiss"))
+        self.lbl_error.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:18pt; color:#2c365d;\">An error has occurred. Try again later.</span></p></body></html>"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
