68565c9b423d2f8c211ea002a4f3d51c29e62205
cnvillarta <48200320+cnvillarta@users.noreply.github.com>
2020-05-13 06:07:52 +0800

    font update

--- Gui/Administrator/Delete Pilot/DeletePilot.py
+++ Gui/Administrator/Delete Pilot/DeletePilot.py
@@ -27,6 +27,10 @@ class Ui_Dialog(object):
         Dialog.setWindowIcon(icon)
         self.btn_cancel = QtWidgets.QPushButton(Dialog)
         self.btn_cancel.setGeometry(QtCore.QRect(70, 130, 151, 31))
+        font = QtGui.QFont()
+        font.setFamily("Helvetica")
+        font.setPointSize(11)
+        self.btn_cancel.setFont(font)
         self.btn_cancel.setStyleSheet("QPushButton {\n"
 "color: rgb(0, 0, 0);\n"
 "background-color: rgb(201, 201, 201);\n"
@@ -52,6 +56,10 @@ class Ui_Dialog(object):
         self.btn_cancel.setObjectName("btn_cancel")
         self.btn_delete = QtWidgets.QPushButton(Dialog)
         self.btn_delete.setGeometry(QtCore.QRect(310, 130, 151, 31))
+        font = QtGui.QFont()
+        font.setFamily("Helvetica")
+        font.setPointSize(11)
+        self.btn_delete.setFont(font)
         self.btn_delete.setStyleSheet("QPushButton {\n"
 "color: rgb(255, 255, 255);\n"
 "background-color: #E53935;\n"
