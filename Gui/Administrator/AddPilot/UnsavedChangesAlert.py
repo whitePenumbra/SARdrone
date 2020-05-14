aa32a54e350492fe9b7e27bc471a35bc40ed5406
cnvillarta <48200320+cnvillarta@users.noreply.github.com>
2020-05-15 00:17:16 +0800

    mainwindow

--- Gui/Administrator/AddPilot/UnsavedChangesAlert.py
+++ Gui/Administrator/AddPilot/UnsavedChangesAlert.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'UnsavedChangesAlert.ui'
+# Form implementation generated from reading ui file 'UnsavedChanges.ui'
 #
 # Created by: PyQt5 UI code generator 5.14.1
 #
@@ -10,18 +10,76 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(531, 191)
-        Dialog.setMinimumSize(QtCore.QSize(531, 191))
-        Dialog.setMaximumSize(QtCore.QSize(531, 191))
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(531, 191)
+        MainWindow.setMinimumSize(QtCore.QSize(531, 191))
+        MainWindow.setMaximumSize(QtCore.QSize(531, 191))
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap("../../Resources/logo_svg.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        Dialog.setWindowIcon(icon)
-        #removes ? from title bar
-        Dialog.setWindowFlags(QtCore.Qt.WindowSystemMenuHint | QtCore.Qt.WindowTitleHint | QtCore.Qt.WindowCloseButtonHint)
-        self.btn_cancel = QtWidgets.QPushButton(Dialog)
+        MainWindow.setWindowIcon(icon)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.btn_delete = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_delete.setGeometry(QtCore.QRect(310, 140, 151, 31))
+        font = QtGui.QFont()
+        font.setFamily("Helvetica")
+        font.setPointSize(11)
+        self.btn_delete.setFont(font)
+        self.btn_delete.setStyleSheet("QPushButton {\n"
+"background-color: rgb(255, 176, 6);\n"
+"border: 1.2px solid #ff9d07;\n"
+"outline: none;\n"
+"}\n"
+"\n"
+"QPushButton:hover{\n"
+"background-color: rgb(255, 157, 7);\n"
+"outline: none;\n"
+"border: none;\n"
+"}\n"
+"\n"
+"QPushButton:pressed{\n"
+"background-color: rgb(254, 140, 8);\n"
+"outline: none;\n"
+"border: none;\n"
+"}")
+        self.btn_delete.setObjectName("btn_delete")
+        self.layoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.layoutWidget.setGeometry(QtCore.QRect(30, 19, 471, 101))
+        self.layoutWidget.setObjectName("layoutWidget")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.layoutWidget)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.label = QtWidgets.QLabel(self.layoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Helvetica")
+        font.setPointSize(21)
+        font.setBold(True)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.verticalLayout_2.addWidget(self.label)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label_2 = QtWidgets.QLabel(self.layoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Helvetica")
+        font.setPointSize(16)
+        self.label_2.setFont(font)
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout.addWidget(self.label_2)
+        self.label_3 = QtWidgets.QLabel(self.layoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Helvetica")
+        font.setPointSize(16)
+        self.label_3.setFont(font)
+        self.label_3.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_3.setObjectName("label_3")
+        self.verticalLayout.addWidget(self.label_3)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+        self.btn_cancel = QtWidgets.QPushButton(self.centralwidget)
         self.btn_cancel.setGeometry(QtCore.QRect(70, 140, 151, 31))
         font = QtGui.QFont()
         font.setFamily("Helvetica")
@@ -50,82 +108,26 @@ class Ui_Dialog(object):
 "\n"
 "")
         self.btn_cancel.setObjectName("btn_cancel")
-        self.btn_delete = QtWidgets.QPushButton(Dialog)
-        self.btn_delete.setGeometry(QtCore.QRect(310, 140, 151, 31))
-        font = QtGui.QFont()
-        font.setFamily("Helvetica")
-        font.setPointSize(11)
-        self.btn_delete.setFont(font)
-        self.btn_delete.setStyleSheet("QPushButton {\n"
-"background-color: rgb(255, 176, 6);\n"
-"border: 1.2px solid #ff9d07;\n"
-"outline: none;\n"
-"}\n"
-"\n"
-"QPushButton:hover{\n"
-"background-color: rgb(255, 157, 7);\n"
-"outline: none;\n"
-"border: none;\n"
-"}\n"
-"\n"
-"QPushButton:pressed{\n"
-"background-color: rgb(254, 140, 8);\n"
-"outline: none;\n"
-"border: none;\n"
-"}")
-        self.btn_delete.setObjectName("btn_delete")
-        self.layoutWidget = QtWidgets.QWidget(Dialog)
-        self.layoutWidget.setGeometry(QtCore.QRect(30, 19, 471, 91))
-        self.layoutWidget.setObjectName("layoutWidget")
-        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.layoutWidget)
-        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout_2.setObjectName("verticalLayout_2")
-        self.label = QtWidgets.QLabel(self.layoutWidget)
-        font = QtGui.QFont()
-        font.setFamily("Helvetica")
-        font.setPointSize(21)
-        self.label.setFont(font)
-        self.label.setAlignment(QtCore.Qt.AlignCenter)
-        self.label.setObjectName("label")
-        self.verticalLayout_2.addWidget(self.label)
-        self.verticalLayout = QtWidgets.QVBoxLayout()
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.label_2 = QtWidgets.QLabel(self.layoutWidget)
-        font = QtGui.QFont()
-        font.setFamily("Helvetica")
-        font.setPointSize(16)
-        self.label_2.setFont(font)
-        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_2.setObjectName("label_2")
-        self.verticalLayout.addWidget(self.label_2)
-        self.label_3 = QtWidgets.QLabel(self.layoutWidget)
-        font = QtGui.QFont()
-        font.setFamily("Helvetica")
-        font.setPointSize(16)
-        self.label_3.setFont(font)
-        self.label_3.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_3.setObjectName("label_3")
-        self.verticalLayout.addWidget(self.label_3)
-        self.verticalLayout_2.addLayout(self.verticalLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, Dialog):
+    def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Unsaved Changes"))
-        self.btn_cancel.setText(_translate("Dialog", "Cancel"))
-        self.btn_delete.setText(_translate("Dialog", "Discard Changes"))
-        self.label.setText(_translate("Dialog", "<html><head/><body><p><span style=\" color:#2c365d;\">Unsaved Changes</span></p></body></html>"))
-        self.label_2.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:13pt; color:#2c365d;\">If you leave this page, any changes you have made will be lost.</span></p></body></html>"))
-        self.label_3.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:13pt; color:#2c365d;\">Are you sure you want to leave this page?</span></p></body></html>"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Unsaved Changes"))
+        self.btn_delete.setText(_translate("MainWindow", "Discard Changes"))
+        self.label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" color:#2c365d;\">Unsaved Changes</span></p></body></html>"))
+        self.label_2.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:13pt; color:#2c365d;\">If you leave this page, any changes you have made will be lost.</span></p></body></html>"))
+        self.label_3.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:13pt; color:#2c365d;\">Are you sure you want to leave this page?</span></p></body></html>"))
+        self.btn_cancel.setText(_translate("MainWindow", "Cancel"))
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
