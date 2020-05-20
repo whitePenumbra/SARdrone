d2994edb8569406a4303d48876256747d896bd08
cnvillarta <48200320+cnvillarta@users.noreply.github.com>
2020-05-20 17:36:46 +0800

    changed button txt

---
+++ Gui/Administrator/AddPilot/UpdatePilotError.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'UpdatePilotError.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(531, 191)
+        MainWindow.setMinimumSize(QtCore.QSize(531, 191))
+        MainWindow.setMaximumSize(QtCore.QSize(531, 191))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("../../Resources/logo_svg.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        MainWindow.setWindowIcon(icon)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.layoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.layoutWidget.setGeometry(QtCore.QRect(20, 19, 491, 121))
+        self.layoutWidget.setObjectName("layoutWidget")
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.layoutWidget)
+        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.label = QtWidgets.QLabel(self.layoutWidget)
+        self.label.setText("")
+        self.label.setPixmap(QtGui.QPixmap("../../Resources/error.png"))
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.verticalLayout_3.addWidget(self.label)
+        self.lbl_error = QtWidgets.QLabel(self.layoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Helvetica")
+        font.setPointSize(19)
+        self.lbl_error.setFont(font)
+        self.lbl_error.setAlignment(QtCore.Qt.AlignCenter)
+        self.lbl_error.setObjectName("lbl_error")
+        self.verticalLayout_3.addWidget(self.lbl_error)
+        self.btn_OK = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_OK.setGeometry(QtCore.QRect(200, 145, 131, 31))
+        self.btn_OK.setMinimumSize(QtCore.QSize(131, 31))
+        self.btn_OK.setMaximumSize(QtCore.QSize(131, 31))
+        font = QtGui.QFont()
+        font.setFamily("Helvetica")
+        font.setPointSize(11)
+        self.btn_OK.setFont(font)
+        self.btn_OK.setStyleSheet("QPushButton {\n"
+"color: rgb(0, 0, 0);\n"
+"background-color: rgb(201, 201, 201);\n"
+"border: 1.2px solid #ABABAB;\n"
+"outline: none;}\n"
+"\n"
+"QPushButton:hover{\n"
+"color: rgb(255, 255, 255);\n"
+"background-color: rgb(171, 171, 171);\n"
+"outline: none;\n"
+"border: none;\n"
+"}\n"
+"\n"
+"QPushButton:pressed{\n"
+"color: rgb(255, 255, 255);\n"
+"background-color: rgb(129, 129, 129);\n"
+"outline: none;\n"
+"border: none;\n"
+"}\n"
+"\n"
+"\n"
+"")
+        self.btn_OK.setObjectName("btn_OK")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Update Pilot Error"))
+        self.lbl_error.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt; color:#2c365d;\">Profile update failed. Try again later</span></p></body></html>"))
+        self.btn_OK.setText(_translate("MainWindow", "OK"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
