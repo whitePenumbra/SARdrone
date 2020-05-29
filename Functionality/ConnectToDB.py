531a72a57a067a109717aeecbeb1701979b17552
Amiel Saballo <saballoamiel@gmail.com>
2020-05-29 12:49:54 +0800

    running test

--- Functionality/ConnectToDB.py
+++ Functionality/ConnectToDB.py
@@ -3,8 +3,8 @@ import MySQLdb as mdb
     
 def connectToDB():
     try:
-        # db = mdb.connect('180.232.102.12', 'aids', '', 'aids-system')
-        db = mdb.connect('localhost', 'root', '', 'aids')
+        db = mdb.connect('180.232.102.12', 'aids', '', 'aids-system')
+        # db = mdb.connect('localhost', 'root', '', 'aids')
 
     except mdb.Error as e:
         print('Connection failed!')
